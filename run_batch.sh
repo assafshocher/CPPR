b036173ab1926037f93e788be1225a8936cae9ef
Assaf <assafshocher@gmail.com>
2022-10-13 14:27:23 -0700

    updated sweep run

--- run_batch.sh
+++ run_batch.sh
@@ -4,24 +4,26 @@ source ~/.bashrc
 
 JOB_DIR=/checkpoint/amirbar/video_mae/logs_dir/
 DATA_PATH=/datasets01/imagenet_full_size/061417
-KILL_AFTER=80
+KILL_AFTER=120
 COUNTER=0
 
 
 
-for DETACH in '' '--detach'
+
+
+for TEMPERATURE in 0.1
 do
-  for W_PRED_LOSS in 0.3333 0.1
+  for W_PRED_LOSS in 0.3333
   do
-    for GROUP_SZ in 49 20 10
+    for BLR in 1.5e-5
     do
       for W_BATCHWISE_LOSS in 0.33333
       do
         for W_PATCHWISE_LOSS in 0.3333
         do
-          for BLR in 1.5e-4
+          for GROUP_SZ in 49
           do
-            for TEMPERATURE in 0.1 1.0
+            for DETACH in '--detach'
             do
                 if ((${COUNTER} > 3)); then
                   partition=learnfair
@@ -29,7 +31,7 @@ do
                   partition=devlab
                 fi
 
-                OUTPUT_DIR="CROSS_temp_${TEMPERATURE}_blr_${BLR}_gsz_${GROUP_SZ}_wpred_${W_PRED_LOSS}_wpred_${W_PRED_LOSS}_wbatch_${W_BATCHWISE_LOSS}_wpatch_${W_PATCHWISE_LOSS}${DETACH}"
+                OUTPUT_DIR="CROSS_tmp${TEMPERATURE}_blr${BLR}_gsz${GROUP_SZ}_wpr${W_PRED_LOSS}_wba${W_BATCHWISE_LOSS}_wpa${W_PATCHWISE_LOSS}${DETACH}"
                 python submitit_pretrain.py \
                         --job_dir ${JOB_DIR}/${OUTPUT_DIR} \
                         --output_dir ${JOB_DIR}/${OUTPUT_DIR}  \
@@ -37,7 +39,122 @@ do
                         --temperature ${TEMPERATURE} \
                         --nodes 1 \
                         --use_volta32 \
-                        --batch_size 128 \
+                        --batch_size 224 \
+                        --model mae_vit_base_patch16 \
+                        --save_ckpt_freq 5 \
+                        --input_size 224 \
+                        --warmup_epochs 40 \
+                        --epochs 800 \
+                        --blr ${BLR} \
+                        --w_pred_loss ${W_PRED_LOSS} \
+                        --w_batchwise_loss ${W_BATCHWISE_LOSS} \
+                        --w_patchwise_loss ${W_PATCHWISE_LOSS} \
+                        ${DETACH} \
+                        --weight_decay 0.05 \
+                        --output_dir ${OUTPUT_DIR}  \
+                        --project_name "linear_prob_comparison" \
+                        --dist_url "file://$OUTPUT_DIR/$TIME" \
+                        --kill_after ${KILL_AFTER} \
+                        --partition ${partition} \
+                        --use_volta32
+                COUNTER=$((COUNTER + 1))
+            done
+          done
+        done
+      done
+    done
+  done
+done
+
+
+
+for TEMPERATURE in 0.1
+do
+  for W_PRED_LOSS in 0.3333
+  do
+    for BLR in 1.5e-5
+    do
+      for W_BATCHWISE_LOSS in 0.33333
+      do
+        for W_PATCHWISE_LOSS in 0.3333
+        do
+          for GROUP_SZ in 49 20 10
+          do
+            for DETACH in ''
+            do
+                if ((${COUNTER} > 3)); then
+                  partition=learnfair
+                else
+                  partition=devlab
+                fi
+
+                OUTPUT_DIR="CROSS_tmp${TEMPERATURE}_blr${BLR}_gsz${GROUP_SZ}_wpr${W_PRED_LOSS}_wba${W_BATCHWISE_LOSS}_wpa${W_PATCHWISE_LOSS}${DETACH}"
+                python submitit_pretrain.py \
+                        --job_dir ${JOB_DIR}/${OUTPUT_DIR} \
+                        --output_dir ${JOB_DIR}/${OUTPUT_DIR}  \
+                        --data_path ${DATA_PATH} \
+                        --temperature ${TEMPERATURE} \
+                        --nodes 1 \
+                        --use_volta32 \
+                        --batch_size 224 \
+                        --model mae_vit_base_patch16 \
+                        --save_ckpt_freq 5 \
+                        --input_size 224 \
+                        --warmup_epochs 40 \
+                        --epochs 800 \
+                        --blr ${BLR} \
+                        --w_pred_loss ${W_PRED_LOSS} \
+                        --w_batchwise_loss ${W_BATCHWISE_LOSS} \
+                        --w_patchwise_loss ${W_PATCHWISE_LOSS} \
+                        ${DETACH} \
+                        --weight_decay 0.05 \
+                        --output_dir ${OUTPUT_DIR}  \
+                        --project_name "linear_prob_comparison" \
+                        --dist_url "file://$OUTPUT_DIR/$TIME" \
+                        --kill_after ${KILL_AFTER} \
+                        --partition ${partition} \
+                        --use_volta32
+                COUNTER=$((COUNTER + 1))
+            done
+          done
+        done
+      done
+    done
+  done
+done
+
+
+
+
+for TEMPERATURE in 0.1 1.0 
+do
+  for W_PRED_LOSS in 0.3333 0.1 1.0
+  do
+    for BLR in 1.5e-5 1e-5 1.5e-4
+    do
+      for W_BATCHWISE_LOSS in 0.33333
+      do
+        for W_PATCHWISE_LOSS in 0.3333
+        do
+          for GROUP_SZ in 49
+          do
+            for DETACH in '' '--detach'
+            do
+                if ((${COUNTER} > 3)); then
+                  partition=learnfair
+                else
+                  partition=devlab
+                fi
+
+                OUTPUT_DIR="CROSS_tmp${TEMPERATURE}_blr${BLR}_gsz${GROUP_SZ}_wpr${W_PRED_LOSS}_wba${W_BATCHWISE_LOSS}_wpa${W_PATCHWISE_LOSS}${DETACH}"
+                python submitit_pretrain.py \
+                        --job_dir ${JOB_DIR}/${OUTPUT_DIR} \
+                        --output_dir ${JOB_DIR}/${OUTPUT_DIR}  \
+                        --data_path ${DATA_PATH} \
+                        --temperature ${TEMPERATURE} \
+                        --nodes 1 \
+                        --use_volta32 \
+                        --batch_size 224 \
                         --model mae_vit_base_patch16 \
                         --save_ckpt_freq 5 \
                         --input_size 224 \
