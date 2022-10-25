e06a25666588b7976060d1ca5efe72d1f4b43ccc
Amir Bar <amirbar@fb.com>
2022-10-24 17:39:16 -0700

    run batch

--- run_batch.sh
+++ run_batch.sh
@@ -46,7 +46,7 @@ do
                         --coeff_bvar ${COEFF_BVAR} \
                         --coeff_pvar ${COEFF_PVAR} \
                         --coeff_fcov ${COEFF_FCOV} \
-                        --coeff_pcross ${COEFF_PCROSS} \
+                        --coeff_pcross ${COEFF_FCOV} \
                         --coeff_var_thr ${COEFF_VAR_THR} \
                         --weight_decay 0.05 \
                         --output_dir ${OUTPUT_DIR}  \
@@ -63,3 +63,63 @@ do
     done
   done
 done
+
+for BLR in 1.5e-5
+do
+  for GROUP_SZ in 49
+  do
+    for COEFF_GINVAR in 25
+    do
+      for COEFF_BVAR in 0.
+      do
+        for COEFF_PVAR in 25
+        do
+          for COEFF_FCOV in 1e4
+          do
+            for COEFF_VAR_THR in 1.
+            do
+                for coeff_pcross in 1e3 1e5
+                do
+                  if ((${COUNTER} > 3)); then
+                    partition=learnfair
+                  else
+                    partition=devlab
+                  fi
+
+                  OUTPUT_DIR="POSCROSS_blr${BLR}_gsz${GROUP_SZ}_ginvar${COEFF_GINVAR}_bvar${COEFF_BVAR}_pvar${COEFF_PVAR}_fcov${COEFF_FCOV}_thr${COEFF_VAR_THR}"
+                  python submitit_pretrain.py \
+                          --job_dir ${JOB_DIR}/${OUTPUT_DIR} \
+                          --output_dir ${JOB_DIR}/${OUTPUT_DIR}  \
+                          --data_path ${DATA_PATH} \
+                          --nodes 1 \
+                          --use_volta32 \
+                          --batch_size 224 \
+                          --model mae_vit_base_patch16 \
+                          --save_ckpt_freq 5 \
+                          --input_size 224 \
+                          --warmup_epochs 40 \
+                          --epochs 800 \
+                          --blr ${BLR} \
+                          --coeff_ginvar ${COEFF_GINVAR} \
+                          --coeff_bvar ${COEFF_BVAR} \
+                          --coeff_pvar ${COEFF_PVAR} \
+                          --coeff_fcov ${COEFF_FCOV} \
+                          --coeff_pcross ${coeff_pcross} \
+                          --coeff_var_thr ${COEFF_VAR_THR} \
+                          --weight_decay 0.05 \
+                          --output_dir ${OUTPUT_DIR}  \
+                          --project_name "linear_prob_comparison" \
+                          --dist_url "file://$OUTPUT_DIR/$TIME" \
+                          --kill_after ${KILL_AFTER} \
+                          --partition ${partition} \
+                          --use_volta32
+                  COUNTER=$((COUNTER + 1))
+                done
+            done
+          done
+        done
+      done
+    done
+  done
+done
+
